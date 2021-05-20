e83ea11cc9af22b87114c6e2c251e83e965dc4b9
Jose Ferrer <estoy@moviendo.me>
2021-05-20 10:49:20 +0700

    fix electron issue with hooks

--- electron-webpack.js
+++ electron-webpack.js
@@ -1,6 +1,7 @@
-const { withExpoAdapter } = require('@expo/electron-adapter');
+const { withExpoAdapter } = require("@expo/electron-adapter");
 
 module.exports = withExpoAdapter({
   projectRoot: __dirname,
+  whiteListedModules: ["easy-peasy"],
   // Provide any overrides for electron-webpack: https://github.com/electron-userland/electron-webpack/blob/master/docs/en/configuration.md
 });
